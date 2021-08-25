5bb4723049e4f773209a5d94d64283c81a5b67c4
FRAZ5094 <frasergmorrison@btinternet.com>
2021-08-25 15:30:16 +0100

    first commit

---
+++ uptime.py
@@ -0,0 +1,25 @@
+filename="Ryzen_log"
+
+def read_file(filename):
+
+    with open(filename) as f:
+        data=f.readlines()
+
+    return data
+
+
+data=read_file(filename)
+
+data_dict={"time":[],"up":[]}
+for i,point in enumerate(data):
+    if point[0]=="[":
+        n=point.find("]")
+        time=float(point[1:n])
+        data_dict["time"].append(time)
+        if "Unreachable" in point:
+            #0=down
+            data_dict["up"].append(0)
+        else:
+            #1=up
+            data_dict["up"].append(1)
+print(data_dict)
