57910b7ca80e84ae949cbd82abe5210ffd54263d
FRAZ5094 <frasergmorrison@btinternet.com>
2021-08-25 16:15:11 +0100

    stamp on x axis

--- uptime.py
+++ uptime.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from datetime import datetime
 filename="Ryzen_log"
 
 def read_file(filename):
@@ -11,12 +12,15 @@ def read_file(filename):
 
 data=read_file(filename)
 
-data_dict={"time":[],"up":[]}
+data_dict={"stamp":[],"epoch":[],"up":[]}
 for i,point in enumerate(data):
     if point[0]=="[":
         n=point.find("]")
-        time=float(point[1:n])
-        data_dict["time"].append(time)
+        epoch=float(point[1:n])
+        data_dict["epoch"].append(epoch)
+        stamp=datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S')
+        data_dict["stamp"].append(stamp)
+
         if "Unreachable" in point:
             #0=down
             data_dict["up"].append(0)
@@ -25,4 +29,5 @@ for i,point in enumerate(data):
             data_dict["up"].append(1)
 
 plt.plot(data_dict["time"],data_dict["up"])
+plt.xticks(data_dict["stamp"]
 plt.show()
